eacb8699969af525e35be8ef02c0ebc163fe95bc
shcodenick <wojtek.kloc@gmail.com>
2023-07-07 16:33:22 +0200

    removed unused

--- variables.tf
+++ variables.tf
@@ -12,10 +12,6 @@ variable AWS_REGION {
   type = string
   default = "us-east-1"
 }
-variable AWS_RDS_PASS {
-  type = string
-  description = "password to postgres"
-}
 variable AWS_STATE_BUCKET {}
 variable AWS_STATE_BUCKET_KEY {}
 variable AWS_ACCESS_KEY_ID {}
